7cc36989eedce16b17d213b9c83773daad39fe31
Johncarpi <a23juan.corredoira@gmail.com>
2023-12-13 14:20:19 +0100

    commit

--- src/RPS.py
+++ src/RPS.py
@@ -9,30 +9,54 @@ class GameAction(IntEnum):
     Scissors = 2
 
 
+class GameResult(IntEnum):
+    Victory = 0
+    Defeat = 1
+    Tie = 2
+
+
+Victories = {
+    GameAction.Rock: GameAction.Paper,
+    GameAction.Paper: GameAction.Scissors,
+    GameAction.Scissors: GameAction.Rock
+}
+
 def assess_game(user_action, computer_action):
+
+    game_result = None
+
     if user_action == computer_action:
         print(f"User and computer picked {user_action.name}. Draw game!")
+        game_result = GameResult.Tie
 
     # You picked Rock
     elif user_action == GameAction.Rock:
         if computer_action == GameAction.Scissors:
             print("Rock smashes scissors. You won!")
+            game_result = GameResult.Victory
         else:
             print("Paper covers rock. You lost!")
+            game_result = GameResult.Defeat
 
     # You picked Paper
     elif user_action == GameAction.Paper:
         if computer_action == GameAction.Rock:
             print("Paper covers rock. You won!")
+            game_result = GameResult.Victory
         else:
             print("Scissors cuts paper. You lost!")
+            game_result = GameResult.Defeat
 
     # You picked Scissors
     elif user_action == GameAction.Scissors:
         if computer_action == GameAction.Rock:
             print("Rock smashes scissors. You lost!")
+            game_result = GameResult.Defeat
         else:
             print("Scissors cuts paper. You won!")
+            game_result = GameResult.Victory
+
+    return game_result
 
 
 def get_computer_action():
